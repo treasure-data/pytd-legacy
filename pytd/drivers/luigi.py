4ac8d951401ed75ad071ac1784c65b37e46e75e9
Keisuke Nishida <knishida@treasure-data.com>
2015-10-30 02:32:12 +0900

    Require users to implement run_query.

--- pytd/drivers/luigi.py
+++ pytd/drivers/luigi.py
@@ -1,3 +1,4 @@
+import os
 import luigi
 
 import logging
@@ -5,22 +6,14 @@ logger = logging.getLogger('luigi-interface')
 
 
 class QueryTask(luigi.Task):
-    time = None
-    variables = {}
-
-    @property
-    def query(self):
-        return NotImplemented()
-
-    def output(self):
-        return luigi.LocalTarget(self.time.strftime("%Y%m%d-") + str(self))
+    def get_output_path(self, context, date=None):
+        if date:
+            return os.path.join('tmp', self.date.strftime("%Y-%m-%d"), context.module, str(self))
+        else:
+            return os.path.join('tmp', context.module, str(self))
 
     def run_query(self):
-        variables = self.variables
-        if self.time:
-            variables = {'scheduled_time': self.time}
-            variables.update(self.variables)
-        return self.query.run(variables, scheduled_time=self.time)
+        raise NotImplemented()
 
     def run(self):
         result = self.run_query()
@@ -34,11 +27,3 @@ class QueryTask(luigi.Task):
             raise RuntimeError("job {0} {1}".format(result.job_id, status))
         with self.output().open('w') as f:
             f.write(str(result.job_id))
-
-
-class DailyQueryTask(QueryTask):
-    time = luigi.DateParameter()
-
-
-class HourlyQueryTask(QueryTask):
-    time = luigi.DateHourParameter()
