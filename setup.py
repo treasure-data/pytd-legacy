271f1e82adef2f74bf6e9128cb8e3af4685584d5
Keisuke Nishida <knishida@treasure-data.com>
2015-11-02 06:53:35 +0900

    Update description

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read_version():
 setup(
     name="pytd",
     version=read_version(),
-    description="Python wrappers for Treasure Data",
+    description="Python wrappers for TD",
     author="Keisuke Nishida",
     author_email="keisuke.nishida@gmail.com",
     url="https://github.com/k24d/pytd",
